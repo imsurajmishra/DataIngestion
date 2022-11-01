d1bb9bcdbd5f0c8b70347f42439ef61ebb24788a
technoddy <mail.technoddy@gmail.com>
2022-10-31 21:06:22 -0700

    comment

--- src/main/java/connection/JDBCConnection.java
+++ src/main/java/connection/JDBCConnection.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 public class JDBCConnection {
 
     public static Connection connect() throws SQLException {
+        // this is for demo but please don't setup username and password in code , instead read from config file or Vault/KMS
         Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/User?serverTimezone=UTC", "root", "root");
         if(connection != null){
                 System.out.println("Connected to database");
